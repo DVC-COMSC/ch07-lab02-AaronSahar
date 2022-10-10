eef0d6101880bf0920c686fa911e895340627778
Aaron Sahar <aaronsahar@gmail.com>
2022-10-10 13:16:15 -0700

    test

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
-# ******************************
-# Make your Code
-# ******************************
+import random
+numbers = []
+for i in range(10):
+    numbers.append(random.randint(0, 100))
+smallest = int(min(numbers))
+index_number = numbers.index(smallest)
+print(smallest)
+print(index_number)
